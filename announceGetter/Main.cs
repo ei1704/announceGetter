2a33f1c5f901f12333543f0988ceaf23418cf554
ei1704 <ryopey1231@gmail.com>
2022-06-12 14:30:37 +0900

    ロード時のウィンドウの設定を追加

--- announceGetter/Main.cs
+++ announceGetter/Main.cs
@@ -16,5 +16,16 @@ namespace announceGetter
         {
             InitializeComponent();
         }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // デスクトップ上の位置とサイズを定義
+            Rectangle bounds = new Rectangle(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width-585, 0, 600, 600);
+            Console.WriteLine(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+            // フォームのデスクトップ上の位置とサイズを設定
+            this.DesktopBounds = bounds;
+            //タイトルバー削除
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
     }
 }
